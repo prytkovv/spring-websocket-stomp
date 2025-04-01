b50196410dc1a1a75d14f217db83d891dce98f60
prytkovv <andreyprytkovv@gmail.com>
2025-04-01 23:34:35 +0300

    add spring session support

--- src/main/java/ru/prytkovv/demo/config/WebSocketConfig.java
+++ src/main/java/ru/prytkovv/demo/config/WebSocketConfig.java
@@ -5,18 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.session.Session;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.session.web.socket.server.SessionRepositoryMessageInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 
 @Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<Session> {
 
     private final String topic;
     private final Long heartbeatIncomingMs;
@@ -24,6 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final Integer maxMessageSizeBytes;
     private final Integer maxBufferSizeBytes;
     private final Integer maxSendTimeLimitMs;
+    private final SessionRepositoryMessageInterceptor<Session> sessionRepositoryMessageInterceptor;
     private TaskScheduler taskScheduler;
 
     public WebSocketConfig(@Value("${app.websocket.topic}") String topic,
@@ -31,13 +35,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                            @Value("${app.websocket.heartbeat.outgoing-ms}") Long heartbeatOutgoingMs,
                            @Value("${app.websocket.max-message-size-bytes}") Integer maxMessageSizeBytes,
                            @Value("${app.websocket.max-buffer-size-bytes}") Integer maxBufferSizeBytes,
-                           @Value("${app.websocket.max-send-time-limit-ms}") Integer maxSendTimeLimitMs) {
+                           @Value("${app.websocket.max-send-time-limit-ms}") Integer maxSendTimeLimitMs,
+                           SessionRepositoryMessageInterceptor<Session> sessionRepositoryMessageInterceptor) {
         this.topic = topic;
         this.heartbeatIncomingMs = heartbeatIncomingMs;
         this.heartbeatOutgoingMs = heartbeatOutgoingMs;
         this.maxMessageSizeBytes = maxMessageSizeBytes;
         this.maxBufferSizeBytes = maxBufferSizeBytes;
         this.maxSendTimeLimitMs = maxSendTimeLimitMs;
+        this.sessionRepositoryMessageInterceptor = sessionRepositoryMessageInterceptor;
     }
 
     @Autowired
@@ -54,8 +60,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ws/products").setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        super.configureClientInboundChannel(registration);
+        registration.interceptors(sessionRepositoryMessageInterceptor);
     }
 
     @Override
@@ -64,4 +76,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setSendBufferSizeLimit(maxBufferSizeBytes);
         registry.setSendTimeLimit(maxSendTimeLimitMs);
     }
+
 }
